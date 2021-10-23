775f17b44b8658e5520c7e37933ceb2c98435efa
tbonnard <thibaud.bonnard@gmail.com>
2021-10-22 21:11:48 -0400

    key config

--- app/config.py
+++ app/config.py
@@ -3,7 +3,10 @@ import os
 
 class Config:
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    uri = os.getenv("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY")
     USE_RELOADER = False
